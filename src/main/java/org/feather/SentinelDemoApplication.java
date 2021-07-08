bd28489de565f1856c56af9e6711092ab69a239e
feather <Brian608@163.com>
2021-07-09 00:14:37 +0800

    :tada:project init

---
+++ src/main/java/org/feather/SentinelDemoApplication.java
@@ -0,0 +1,17 @@
+package org.feather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: sentinelDemo
+ * @description:
+ * @author: 杜雪松(feather)
+ * @create: 2021-07-09 00:11
+ **/
+@SpringBootApplication
+public class SentinelDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelDemoApplication.class);
+    }
+}
